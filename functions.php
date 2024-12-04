866d365ecfb1af579a68fa8c1ab4fe75e43d8f11
Jasper Frumau <jasper@imagewize.com>
2024-12-04 10:21:11 +0700

    cookieYes dns-prefecth path + emoji removal

--- functions.php
+++ functions.php
@@ -26,16 +26,17 @@ add_action('wp_enqueue_scripts', 'base_hotel_child_enqueue_styles');
 
 
 /**
- * Add DNS prefetch and preconnect for external domains
- * Improves initial connection time for external resources
+ * Add DNS prefetch for CookieYes domains before any scripts load
  */
-add_action('wp_head', function () {
+function add_cookieyes_dns_prefetch() {
     $domains = [
         'cdn-cookieyes.com',
         'directory.cookieyes.com',
         'log.cookieyes.com'
     ];
     
+    // Buffer output to ensure clean HTML
+    ob_start();
     foreach ($domains as $domain) {
         printf(
             '<link rel="dns-prefetch" href="//%1$s">'."\n".
@@ -43,4 +44,57 @@ add_action('wp_head', function () {
             esc_attr($domain)
         );
     }
-}, 1);
+    $output = ob_get_clean();
+    
+    // Echo at the very start of head
+    echo "<!-- DNS Prefetch for CookieYes -->\n" . $output;
+}
+
+// Add DNS prefetch as early as possible
+add_action('wp_head', 'add_cookieyes_dns_prefetch', -1);
+
+// Remove all other CookieYes customizations
+remove_action('wp_head', 'optimize_cookieyes_loading', 1);
+remove_action('wp_head', 'add_cookieyes_script', 2);
+remove_action('wp_enqueue_scripts', 'modify_cookieyes_script', 999);
+
+function disable_emoji_feature() {
+
+    // Prevent Emoji from loading on the front-end
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
+    // Remove from admin area also
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+    // Remove from RSS feeds also
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji');
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji');
+
+    // Remove from Embeds
+    remove_filter( 'embed_head', 'print_emoji_detection_script' );
+
+    // Remove from emails
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+
+    // Disable from TinyMCE editor. Currently disabled in block editor by default
+    add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+
+    /** Finally, prevent character conversion too
+         ** without this, emojis still work 
+         ** if it is available on the user's device
+     */
+
+    add_filter( 'option_use_smilies', '__return_false' );
+
+}
+
+function disable_emojis_tinymce( $plugins ) {
+    if( is_array($plugins) ) {
+        $plugins = array_diff( $plugins, array( 'wpemoji' ) );
+    }
+    return $plugins;
+}
+
+add_action('init', 'disable_emoji_feature');
